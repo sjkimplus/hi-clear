f1a33e8435ecacebf00ddfc55d6de67384ec4d11
KangDongJoon <ehdwns1119@naver.com>
2024-10-24 11:59:14 +0900

    [FEAT] SpringSecurity를 통한 인증/인가 구현

---
+++ src/main/java/com/play/hiclear/config/JwtAuthenticationToken.java
@@ -0,0 +1,26 @@
+package com.play.hiclear.config;
+
+import com.play.hiclear.domain.auth.entity.AuthUser;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final AuthUser authUser;
+
+    public JwtAuthenticationToken(AuthUser authUser) {
+        super(authUser.getAuthorities());
+        this.authUser = authUser;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return authUser;
+    }
+}
+
