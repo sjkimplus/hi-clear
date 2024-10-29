47f470d48c18c07c4c349a5c92ca184e87f21df3
KangDongJoon <ehdwns1119@naver.com>
2024-10-29 19:52:34 +0900

    [FIX] 코드 리펙토링

--- src/main/java/com/play/hiclear/domain/timeslot/sevice/TimeSlotService.java
+++ src/main/java/com/play/hiclear/domain/timeslot/sevice/TimeSlotService.java
@@ -16,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
