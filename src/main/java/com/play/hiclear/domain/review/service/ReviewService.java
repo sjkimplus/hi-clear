0032154320b8076cbe26412d53fd0cc5087fd0f6
Sungju Kim <sjkim20010909@gmail.com>
2024-11-19 23:49:27 +0900

    [ADD] ElasticSearch 기능추가

--- src/main/java/com/play/hiclear/domain/review/service/ReviewService.java
+++ src/main/java/com/play/hiclear/domain/review/service/ReviewService.java
@@ -24,7 +24,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
